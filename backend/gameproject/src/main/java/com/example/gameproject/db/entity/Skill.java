c9592df771d72b21d33c579d863d30fd94269438
고병진 <gotommao@gmail.com>
2023-03-08 17:56:25 +0900

    엔티티 작성중

---
+++ backend/gameproject/src/main/java/com/example/gameproject/db/entity/Skill.java
@@ -0,0 +1,17 @@
+package com.example.gameproject.db.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Skill {
+    private String skillName;
+    private int skillType;
+    private boolean isRange;
+    private int value;
+    private  boolean isEnemy;
+    private int target;
+    private int coolDown;
+    private User user;
+
+
+}
