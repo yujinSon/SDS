74d9ce584a1ddd5aea532b72b95be395b5c60b80
손유진 <lyl0896@naver.com>
2023-03-08 15:58:08 +0900

    [chore] 프로젝트 구조 작성

---
+++ backend/gameproject/src/main/java/com/example/gameproject/GameprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameprojectApplication.class, args);
+	}
+
+}
