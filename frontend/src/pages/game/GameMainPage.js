8d64e343ef9b6aa263bdcb8c40d3cc541c44a0f3
Minhyeok Son <mh.blosson@gmail.com>
2023-03-15 14:45:17 +0900

    [feat][FE] 게임 페이지 이미지 삭제

--- frontend/src/pages/game/GameMainPage.js
+++ frontend/src/pages/game/GameMainPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Son from 'assets/img/손민혁.png';
 
 export default function GameMainPage() {
   return (
     <div>
       <div>GameMainPage</div>;
-      <img src={Son} alt="" />
     </div>
   );
 }
