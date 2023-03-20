d231458a4236289ba966f578a65ab2f4edb5365a
고병진 <zerg758@naver.com>
2023-03-20 13:55:57 +0900

    [feat: add crossorigin]

--- backend/gameproject/src/main/java/com/example/gameproject/api/controller/UserController.java
+++ backend/gameproject/src/main/java/com/example/gameproject/api/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
