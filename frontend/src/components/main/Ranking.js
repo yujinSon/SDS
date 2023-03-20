059c7699807f01e50af55f3524f18d83aa1d3457
함정빈 <shadberry02@naver.com>
2023-03-20 11:03:57 +0900

    Deleted frontend/public/favicon.ico, frontend/public/index.html, frontend/public/logo192.png, frontend/public/logo512.png, frontend/public/manifest.json, frontend/public/robots.txt, frontend/src/assets/img/battle.png, frontend/src/assets/img/bg-mainpage.png, frontend/src/assets/img/frame.png, frontend/src/assets/img/map.png, frontend/src/assets/img/map2.png, frontend/src/assets/img/고병진.png, frontend/src/assets/img/손민혁.png, frontend/src/components/auth/Kakao.js, frontend/src/components/common/Button.js, frontend/src/components/common/IconButton.js, frontend/src/components/common/Modal.js, frontend/src/components/common/Navbar.js, frontend/src/components/game/Battle.js, frontend/src/components/game/CharacterDetail.js, frontend/src/components/game/GameButtons.js, frontend/src/components/game/Information.js, frontend/src/components/game/RandomCharacterList.js, frontend/src/components/game/SelectedCharacterList.js, frontend/src/components/main/Main.js, frontend/src/components/main/Ranking.js, frontend/src/components/main/Tutorial.js, frontend/src/constants/api.js, frontend/src/constants/pk.js, frontend/src/layouts/GamePageLayout.js, frontend/src/layouts/MainPageLayout.js, frontend/src/libs/axios.js, frontend/src/pages/game/BattlePage.js, frontend/src/pages/game/GameMainPage.js, frontend/src/pages/game/MapPage.js, frontend/src/pages/main/MainPage.js, frontend/src/styles/GlobalStyle.js, frontend/src/styles/font.css, frontend/src/styles/theme.js, frontend/src/.prettierrc, frontend/src/App.css, frontend/src/App.js, frontend/src/App.test.js, frontend/src/index.css, frontend/src/index.js, frontend/src/reportWebVitals.js, frontend/src/setupTests.js, frontend/.gitignore, frontend/README.md, frontend/jsconfig.json, frontend/package-lock.json, frontend/package.json, exec/기능_명세서.pdf

--- frontend/src/components/main/Ranking.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Ranking() {
-  return (
-    <div>
-      마 이게 랭킹이다!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    </div>
-  );
-}
--- frontend/src/components/main/Tutorial.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import sampleImg from 'assets/img/고병진.png';
-
-export default function Tutorial() {
-  return <img src={sampleImg} alt="Tutorial Img"></img>;
-}
