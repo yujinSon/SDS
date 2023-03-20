58c8336b2c943a3a3062633da48aac3372a98a20
고병진 <zerg758@naver.com>
2023-03-20 15:20:49 +0900

    feat : update annotation, Dockerfile

--- backend/gameproject/src/main/java/com/example/gameproject/api/controller/UserController.java
+++ backend/gameproject/src/main/java/com/example/gameproject/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.gameproject.api.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins="*")
 @RestController
