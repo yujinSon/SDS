19b71d9f4cbb68ef37d324d0affb31ad35a2f67a
Minhyeok Son <mh.blosson@gmail.com>
2023-03-22 16:40:18 +0900

    [FE][feat] 전투 페이지 클릭, 캐릭터 공격 기능 구현

--- frontend/src/pages/game/BattlePage.js
+++ frontend/src/pages/game/BattlePage.js
@@ -7,9 +7,6 @@ import api from 'constants/api';
 import Battle from 'components/game/Battle';
 import Information from 'components/game/Information';
 
-import IMG from 'assets/img/고병진.png';
-import IMG2 from 'assets/img/손민혁.png';
-
 export default function BattlePage() {
   useEffect(() => {
     const [url, method] = api('loadStage');
@@ -17,94 +14,63 @@ export default function BattlePage() {
     axios(config)
       .then((res) => {
         console.log('전투페이지 불러오기', res.data);
+        setCharacters(res.data.character);
+        setMonsters(res.data.villain);
       })
       .catch((err) => {});
   }, []);
 
-  const [characters, setCharacters] = useState([
-    {
-      name: '경찰 병진',
-      level: 7,
-      top: 10,
-      left: 20,
-      hp: 120,
-      ad: 70,
-      ap: 30,
-      pos: 0,
-      skillImg: [
-        [IMG2, '공격력 2 증가'],
-        [IMG2, '체력 3 증가'],
-        [IMG, 'AP 20증가'],
-      ],
-    },
-    {
-      name: '비혼주의자 민혁',
-      level: 3,
-      top: 30,
-      left: 8,
-      hp: 160,
-      ad: 30,
-      ap: 80,
-      pos: 1,
-      skillImg: [
-        [IMG, '공격력 24 증가'],
-        [IMG2, '체력 351 증가'],
-        [IMG2, 'AP 203 증가'],
-      ],
-    },
-    {
-      name: '예비군 용찬',
-      level: 4,
-      top: 55,
-      left: 20,
-      hp: 670,
-      ad: 50,
-      ap: 37,
-      pos: 2,
-      skillImg: [
-        [IMG, '공격력 122 증가'],
-        [IMG2, '체력 334 증가'],
-        [IMG, 'AP 205 증가'],
-      ],
-    },
-  ]);
+  const [characters, setCharacters] = useState(null);
+  const [monsters, setMonsters] = useState(null);
 
-  const [monsters, setMonsters] = useState([
-    {
-      name: '턱스크 빌런',
-      top: 10,
-      left: 80,
-      pos: 4,
-    },
-    {
-      name: '신종플루 걸린 개',
-      top: 30,
-      left: 92,
-      pos: 5,
-    },
-    {
-      name: '감염된 의사',
-      top: 45,
-      left: 80,
-      pos: 6,
-    },
-    {
-      name: '메르스 감염자',
-      top: 55,
-      left: 92,
-      pos: 7,
-    },
-  ]);
+  const [turnOrder, setTurnOrder] = useState(null);
+  // 현재 턴인 pos
+  const [nowTurn, setNowTurn] = useState(null);
+  // turnOrder 배열의 인덱스
+  const [nowIdx, setNowIdx] = useState(0);
+
+  // speed에 따라 공격 turn 계산하기
+  useEffect(() => {
+    if (!characters | !monsters) return;
+    const mergedArr = [
+      ...characters.map(({ pos, speed }) => ({ pos, speed })),
+      ...monsters.map(({ pos, speed }) => ({ pos, speed })),
+    ];
+    mergedArr.sort((a, b) => b.speed - a.speed);
+    // 현재 턴 박스 표시해주기 (캐릭터 바뀌면 수정할 수 밖에 없음..)
+    setNowTurn(mergedArr[nowIdx].pos);
+    setTurnOrder(mergedArr);
+  }, [characters, monsters]);
+  // console.log('턴', turnOrder);
+
+  // 턴 순서에 따라서 공격 logic 실행
+  useEffect(() => {
+    if (!turnOrder) return;
+    setNowTurn(turnOrder[nowIdx].pos);
+    // console.log('현재 공격 pos', turnOrder[nowIdx]);
+  }, [nowIdx]);
 
   // playerTurn이 2가 된 상태에서 몬스터를 클릭하면 공격하는 것으로 간주
   const [playerTurn, setPlayerTurn] = useState(0);
   const [selectedCh, setSelectedCh] = useState(null);
   const [selectedSkill, setSelectedSkill] = useState(null);
+  const [attackedMonster, setAttackedMonster] = useState(null);
 
   const clickCh = (ch) => {
-    setSelectedCh(ch.pos);
-    setSelectedSkill(null);
-    setPlayerTurn(1);
+    // 버프 주는 거임
+    if (playerTurn === 2) {
+      console.log('옛다 버프다~');
+      setPlayerTurn(0);
+      setNowIdx(nowIdx + 1);
+    } else {
+      if (ch.pos === nowTurn) {
+        setSelectedCh(ch.pos);
+        setSelectedSkill(null);
+        setPlayerTurn(1);
+      } else {
+        console.log('응 니턴 아니야~');
+      }
+    }
   };
 
   const clickSkill = (idx) => {
@@ -112,7 +78,18 @@ export default function BattlePage() {
     if (playerTurn === 1) {
       setPlayerTurn(2);
     }
-    console.log(playerTurn);
+  };
+
+  const clickMonster = (pos) => {
+    if (playerTurn !== 2) return;
+
+    console.log('공격당한 몬스터 pos', pos);
+    setPlayerTurn(0);
+
+    // 플레이어가 공격했으면 다음 턴으로 넘어감
+    setNowIdx(nowIdx + 1);
+    // 현재 몇 번재 턴인지 출력
+    console.log('내가 방금 공격한 턴', nowIdx);
   };
 
   return (
@@ -123,17 +100,22 @@ export default function BattlePage() {
           monsters={monsters}
           selectedCh={selectedCh}
           clickCh={clickCh}
+          clickMonster={clickMonster}
+          nowTurn={nowTurn}
         />
       </BattleContainer>
       <BottomContainer>
-        <Information
-          character={characters[selectedCh]}
-          selectedSkill={selectedSkill}
-          setSelectedSkill={setSelectedSkill}
-          clickSkill={clickSkill}
-        >
-          여기는 왼쪽 아래
-        </Information>
+        {(selectedCh === 0) | selectedCh ? (
+          <Information
+            character={characters[selectedCh]}
+            selectedSkill={selectedSkill}
+            setSelectedSkill={setSelectedSkill}
+            clickSkill={clickSkill}
+          >
+            여기는 왼쪽 아래
+          </Information>
+        ) : null}
+
         <RightContainer>여기는 오른쪽 아래</RightContainer>
       </BottomContainer>
     </MainContainer>
