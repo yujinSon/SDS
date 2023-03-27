b62b79cdbcf2c007a73cac79e6f9f380e122f9d0
Diligent0924 <sdc00039@gmail.com>
2023-03-27 14:51:03 +0900

    [Fix] Nginx 연결을 위한 구조 변환-1

--- backend/gameproject/src/main/java/com/example/gameproject/api/controller/CharacterController.java
+++ backend/gameproject/src/main/java/com/example/gameproject/api/controller/CharacterController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/character")
+@RequestMapping("/api/character")
 public class CharacterController {
     @Autowired
     private CharacterService characterService;
