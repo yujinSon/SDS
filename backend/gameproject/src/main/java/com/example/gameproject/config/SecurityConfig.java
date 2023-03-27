3c8d436a2c3b7fb8f789ae39a65193dd138580ba
Diligent0924 <sdc00039@gmail.com>
2023-03-27 11:13:03 +0900

    [Feat] Post를 위한 http.csrf().disable() 추가

--- backend/gameproject/src/main/java/com/example/gameproject/config/SecurityConfig.java
+++ backend/gameproject/src/main/java/com/example/gameproject/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.csrf().disable();
         http.authorizeRequests()
 //                .antMatchers("/admin/**").hasRole("ROLE_ADMIN")
                 .anyRequest().permitAll()
