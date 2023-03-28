c2f5939ab0dae8f1b088fd049e6efb3e9526ec0e
Diligent0924 <sdc00039@gmail.com>
2023-03-28 17:02:44 +0900

    설정 변경

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -32,7 +32,7 @@ export default function Main() {
   // 카카오 로그인
   const kakaoLogin = () => {
     axios({
-      url: 'https://j8a303.p.ssafy.io/login',
+      url: 'https://j8a303.p.ssafy.io/api/oauth/loginInfo',
       method: 'post',
       // withCredentials: true,
     })
