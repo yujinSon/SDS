0f011596a628053bd05eb3f0923c6f7cfe3005f6
Minhyeok Son <mh.blosson@gmail.com>
2023-03-29 17:38:30 +0900

    [FE][feat] 상점 페이지 구현, step 2 클리어 시 상점페이지로 navigate 이동

--- frontend/src/pages/game/ShopPage.js
+++ frontend/src/pages/game/ShopPage.js
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'libs/axios';
+import api from 'constants/api';
+
+import Modal from 'components/common/Modal';
+import ItemModal from 'components/game/ItemModal';
+import RecoveryModal from 'components/game/RecoveryModal';
 
 import 영입 from 'assets/img/영입.png';
 import 휴식 from 'assets/img/휴식.png';
@@ -10,29 +18,68 @@ import 휴식버튼 from 'assets/img/휴식버튼.png';
 import 유물버튼 from 'assets/img/유물버튼.png';
 
 export default function ShopPage() {
+  const navigate = useNavigate();
+
+  const [recoveryModal, setRecoveryModal] = useState(false);
+  const [itemModal, setItemModal] = useState(false);
+
+  useEffect(() => {}, []);
+
   return (
     <Container>
-      <img src={상점} alt="" />
+      <img src={상점} alt="상점" />
       <Card>
-        <CardItem>
-          <img src={영입} alt="" />
+        <CardItem onClick={() => navigate('/main')}>
+          <img src={영입} alt="영입" />
           <button>
-            <img src={영입버튼} alt="" />
+            <img src={영입버튼} alt="영입버튼" />
           </button>
         </CardItem>
-        <CardItem>
-          <img src={휴식} alt="" />
+        <CardItem
+          onClick={() => {
+            setRecoveryModal(!recoveryModal);
+            const [url, method] = api('rest');
+            const config = { url, method };
+            axios(config)
+              .then((res) => {
+                console.log('상점에서 캐릭터 체력 회복 성공', res.data);
+              })
+              .catch((err) => {
+                console.log('상점에서 캐릭터 체력 회복 에러', err);
+              });
+          }}
+        >
+          <img src={휴식} alt="휴식" />
           <button>
-            <img src={휴식버튼} alt="" />
+            <img src={휴식버튼} alt="휴식버튼" />
           </button>
         </CardItem>
-        <CardItem>
-          <img src={유물} alt="" />
+        <CardItem
+          onClick={() => {
+            setItemModal(!itemModal);
+            const [url, method] = api('addItem');
+            const config = { url, method };
+            axios(config)
+              .then((res) => {
+                console.log('상점에서 유물 획득 성공', res.data);
+              })
+              .catch((err) => {
+                console.log('상점에서 유물 획득 에러', err);
+              });
+          }}
+        >
+          <img src={유물} alt="유물" />
           <button>
-            <img src={유물버튼} alt="" />
+            <img src={유물버튼} alt="유물버튼" />
           </button>
         </CardItem>
       </Card>
+      {recoveryModal ? (
+        <Modal close={() => navigate('/main')} content={<RecoveryModal />} />
+      ) : null}
+      {itemModal ? (
+        <Modal close={() => navigate('/main')} content={<ItemModal />} />
+      ) : null}
     </Container>
   );
 }
