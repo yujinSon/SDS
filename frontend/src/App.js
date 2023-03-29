b8a6ee4e699c588076bf98625f173778c44cbca2
Minhyeok Son <mh.blosson@gmail.com>
2023-03-29 15:27:13 +0900

    [FE][feat] 게임 대기 페이지 유물 컴포넌트 구조 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,9 @@ import MapPage from 'pages/game/MapPage';
 import BattlePage from 'pages/game/BattlePage';
 import EndingPage from 'pages/game/EndingPage';
 
+// 프레임만 설정하고 나중에 지울거임 (03.29 민혁)
+import ItemPage from 'pages/game/ItemPage';
+
 function App() {
   return (
     <>
@@ -21,6 +24,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/main" element={<GameMainPageLayout />}>
             <Route path="" element={<GameMainPage />} />
+            <Route path="item" element={<ItemPage />} />
           </Route>
           <Route path="/map" element={<MapPage />} />
           <Route path="/battle" element={<BattlePage />} />
