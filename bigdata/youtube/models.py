df01abb0c8163a27d8bbd45179b36f038a4a4cee
함정빈 <shadberry02@naver.com>
2023-03-30 15:15:20 +0900

    feat : db 변경

--- bigdata/youtube/models.py
+++ bigdata/youtube/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Youtube(models.Model):
-    id = models.IntegerField(primary_key=True)
-    comment = models.CharField(max_length=300, blank=True, null=True)
+    id = models.BigAutoField(primary_key=True)
+    comment = models.CharField(max_length=10000, blank=True, null=True)
     author = models.CharField(max_length=45, blank=True, null=True)
     date = models.DateTimeField(blank=True, null=True)
     likes = models.IntegerField(blank=True, null=True)
+    subject = models.CharField(max_length=20, null=True)
 
 class Artifact(models.Model):
     id = models.BigAutoField(primary_key=True)
