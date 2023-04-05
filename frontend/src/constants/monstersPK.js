2373d8952287df3255c625e35c759e9fb7cd187d
Minhyeok Son <mh.blosson@gmail.com>
2023-04-05 11:22:45 +0900

    [FE][feat] 빌런 이미지 적용

---
+++ frontend/src/constants/monstersPK.js
@@ -0,0 +1,67 @@
+// import 지구온난화 from 'assets/img/monsters/지구 온난화.PNG';
+// import 수질오염 from 'assets/img/monsters/수질 오염.PNG';
+// import 대기오염 from 'assets/img/monsters/대기 오염.PNG';
+// import 토양오염 from 'assets/img/monsters/토양 오염.PNG';
+
+import 핵 from 'assets/img/monsters/핵.PNG';
+import 탱크 from 'assets/img/monsters/탱크.PNG';
+import 무인기 from 'assets/img/monsters/무인기.PNG';
+import 총 from 'assets/img/monsters/총.PNG';
+
+import 코로나 from 'assets/img/monsters/코로나.PNG';
+import 메르스 from 'assets/img/monsters/메르스.PNG';
+import 콜레라 from 'assets/img/monsters/콜레라.PNG';
+import 조류독감 from 'assets/img/monsters/조류 독감.PNG';
+
+import 음주운전 from 'assets/img/monsters/음주운전.PNG';
+import 인종차별 from 'assets/img/monsters/인종차별.PNG';
+import 알콜중독 from 'assets/img/monsters/알콜중독.PNG';
+import 사기 from 'assets/img/monsters/사기.PNG';
+
+import 살인 from 'assets/img/monsters/살인.PNG';
+import 학교폭력 from 'assets/img/monsters/학교폭력.PNG';
+import 마약 from 'assets/img/monsters/마약.PNG';
+import 도박 from 'assets/img/monsters/도박.PNG';
+
+import 저출산 from 'assets/img/monsters/저출산.PNG';
+import 노령화 from 'assets/img/monsters/노령화.PNG';
+import 재정악화 from 'assets/img/monsters/재정악화.PNG';
+import 과도한노동 from 'assets/img/monsters/과도한 노동.PNG';
+
+import 저성장 from 'assets/img/monsters/저성장.PNG';
+import 경제불균형 from 'assets/img/monsters/경제 불균형.PNG';
+import 고용불안 from 'assets/img/monsters/고용 불안.PNG';
+import 의욕상실 from 'assets/img/monsters/의욕 상실.PNG';
+
+const monstersPK = {
+  지구온난화: 핵,
+  살인,
+  저출산,
+  저성장,
+  수질오염: 탱크,
+  대기오염: 총,
+  토양오염: 무인기,
+  탱크,
+  무인기,
+  총,
+  메르스,
+  콜레라,
+  조류독감,
+  인종차별,
+  알콜중독,
+  사기,
+  학교폭력,
+  마약,
+  도박,
+  노령화,
+  음주운전,
+  코로나,
+  '핵(Nuclear)': 핵,
+  '재정 악화': 재정악화,
+  '과도한 노동': 과도한노동,
+  '경제 불균형': 경제불균형,
+  '고용 불안': 고용불안,
+  '의욕 상실': 의욕상실,
+};
+
+export default monstersPK;
