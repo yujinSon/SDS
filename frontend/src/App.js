2cb3e34d9f3a235a55dd03ea2d7bd662cb3ed0a4
고병진 <gotommao@gmail.com>
2023-04-06 17:41:09 +0900

    [UPDATE][FE] 상점으로 뒤로가기, 새로고침 버튼 차단

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate, useLocation, useBlocker  } from 'react-router-dom';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { theme } from 'styles/theme';
 
@@ -20,6 +20,31 @@ import ItemPage from 'pages/game/ItemPage';
 import RecruitPage from 'pages/game/RecruitPage';
 
 function App() {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleBackButton = () => {
+      // 원하는 페이지로 이동
+      navigate("/game/ready");
+    };
+
+    const previousPath = sessionStorage.getItem("previousPath");
+
+    if (previousPath && previousPath === location.pathname) {
+      handleBackButton();
+    }
+
+    if (previousPath && previousPath === "/shop") {
+      handleBackButton();
+    }
+
+    sessionStorage.setItem("previousPath", location.pathname);
+
+  }, [location, navigate]);
+
+
   return (
     <>
       <GlobalStyle />
